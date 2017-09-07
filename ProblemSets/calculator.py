6a173a7b585e5cfa8d7bcdc02861a66e6e7bebcf
jiarui.guo@maec.moore.sc.edu <jiarui.guo@maec.moore.sc.edu>
2017-09-07 09:15:59 -0400

    This is the calculator module used in PS2.

---
+++ ProblemSets/calculator.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[43]:
+
+def pro(a,b):
+    c=a*b
+    return c
+    
+    
+def sqrt(c):
+    d=c**(1/2)
+    return d
+   
+
+
+# In[44]:
+
+pro(3**2,4**2)
+
+
+# In[45]:
+
+sqrt(pro(3**2,4**2))
+
