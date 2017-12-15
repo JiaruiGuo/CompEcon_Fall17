b5e65dfc34dca1798d031062c305ad8b0fe6449c
jiarui.guo@maec.moore.sc.edu <jiarui.guo@maec.moore.sc.edu>
2017-12-14 23:41:05 -0500

    submission for PS_9

---
+++ ProblemSets/PS9_Guo/firm_exte_fina.py
@@ -0,0 +1,85 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy as np
+from scipy.stats import norm
+import scipy.integrate as integrate
+import pickle as pk
+import pandas as pd
+
+
+
+
+
+# In[5]:
+
+
+def firm_sol(alpha,delta, psi,r,z,pi,phi):
+    
+    betafirm = (1 / (1 + r))
+    dens = 5
+# put in bounds here for the capital stock space
+    
+    kbar = ((1/betafirm-1+delta)/alpha)**(1/(alpha-1))
+    lb_k = 0.001
+    ub_k = kbar
+    krat = np.log(lb_k / ub_k)
+    numb = np.ceil(krat / np.log(1 - delta))
+    K = np.zeros(int(numb * dens))
+# we'll create in a way where we pin down the upper bound - since
+# the distance will be small near the lower bound, we'll miss that by little
+    for j in range(int(numb * dens)):
+        K[j] = ub_k * (1 - delta) ** (j / dens)
+    kgrid = K[::-1]
+    sizek = kgrid.shape[0]
+    op=np.ones([sizek,len(z)])
+# operating profits, op
+
+    for i in range(len(z)):
+    
+        op[:,i] = z[i]*kgrid**alpha 
+
+# firm cash flow, e
+    E = np.zeros((len(z),sizek, sizek))
+    for k in range(len(z)):
+        for i in range(sizek):
+            for j in range(sizek):
+        
+                E[k, i,j] = (op[i,k] - kgrid[j] + ((1 - delta) * kgrid[i]) -
+                       ((psi / 2) * ((kgrid[j] - ((1 - delta) * kgrid[i])) ** 2)/ kgrid[i]))
+    I=(E>0)
+    e=E*I+(1-I)*(E-phi)            
+    VFtol = 1e-6
+    VFdist = 7.0
+    VFmaxiter = 3000
+    V = np.zeros((len(z), sizek))  # initial guess at value function
+    Vmat = np.zeros((len(z),sizek, sizek))  # initialize Vmat matrix
+    Vstore = np.zeros((len(z),sizek,  VFmaxiter))  # initialize Vstore array
+    VFiter = 1
+    while VFdist > VFtol and VFiter < VFmaxiter:
+        TV = V
+        expected_V=np.dot(pi,V)
+        for i in range(sizek):  # loop over k
+            Vmat[:,i,: ] = e[:,i, :] + betafirm * expected_V
+
+        Vstore[:,:, VFiter] = V.reshape(len(z),sizek,)  # store value function at each
+    # iteration for graphing later
+        V = Vmat.max(axis=2)  # apply max operator to Vmat (to get V(k))
+        PF = np.argmax(Vmat, axis=2)  # find the index of the optimal k'
+        Vstore[:, :,i] = V  # store V at each iteration of VFI
+        VFdist = (np.absolute(V - TV)).max()  # check distance between value
+    # function for this iteration and value function from past iteration
+        VFiter += 1
+    Firm_Solution={'PF': PF, 'pi':pi,'z':z,'kvec':kgrid}
+    filehandler = open("firm_solution.obj","wb")
+    pk.dump(Firm_Solution,filehandler)
+    filehandler.close()
+    return Firm_Solution,e, V, I    
+
+
+# In[ ]:
+
+
+
